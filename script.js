d66e4b0cfd70481f2dbe2cad5a0b62be27abd4bf
PJ <boyle.peter614@gmail.com>
2022-04-28 17:32:15 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const bg = document.getElementById('bg');
+const fg = document.getElementById('fg');
+const mount = document.getElementById('mount');
+const nameIs = document.getElementById('nameIs');
+const jag = document.getElementById('jag');
+const jung = document.getElementById('jung');
+const about = document.getElementById('aboutSection');
+const fadeIn = document.querySelectorAll('.fade-in')
+
+window.addEventListener('scroll', function(){
+    const valueY = window.scrollY;
+    bg.style.top = valueY * 0.9 + 'px';
+    mount.style.top = valueY * 0.5 + 'px';
+});
+
+const fadeInOptions = {
+    threshold: 1,
+    rootMargin: "0px 0px -100px 0px"
+};
