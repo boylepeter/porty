9ab935b483b5b63f57dc25c9efe68dfd436e9055
PJ <boyle.peter614@gmail.com>
2022-11-09 10:40:55 -0500

    z index for intro fix

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ const bg = document.getElementById('bg');
 const fg = document.getElementById('fg');
 const mount = document.getElementById('mount');
 const nameIs = document.getElementById('nameIs');
-const jag = document.getElementById('jag');
-const jung = document.getElementById('jung');
 const about = document.getElementById('aboutSection');
 const fadeIn = document.querySelectorAll('.fade-in')
 
@@ -11,6 +9,7 @@ window.addEventListener('scroll', function(){
     const valueY = window.scrollY;
     bg.style.top = valueY * 0.9 + 'px';
     mount.style.top = valueY * 0.5 + 'px';
+    nameIs.style.top = valueY * 0.5 + 'px';
 });
 
 const fadeInOptions = {
